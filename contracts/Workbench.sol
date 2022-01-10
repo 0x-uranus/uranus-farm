4d34f66ab9b99ec27e9b140a69470dc7dffad955
dream-builder48 <74439927+dream-builder48@users.noreply.github.com>
2022-01-10 22:07:01 +0300

    uranus-farm-version1

---
+++ contracts/Workbench.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GEO
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./TANToken.sol";
+
+// import "./lib/token/ERC20/ERC20.sol";
+// import "./lib/access/Ownable.sol";
+
+
+// Workbench with Governance.
+contract Workbench is ERC20('Workbench Token', 'BENCH'), Ownable {
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (Craftsman).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from ,uint256 _amount) public onlyOwner {
+        _burn(_from, _amount);
+    }
+
+    // The TAN TOKEN!
+    TANToken public TAN;
+
+    constructor (TANToken _TAN) public {
+        TAN = _TAN;
+    }
+
+    // Safe TAN transfer function, just in case if rounding error causes pool to not have enough TANs.
+    function safeTANTransfer(address _to, uint256 _amount) public onlyOwner {
+        uint256 TANBal = TAN.balanceOf(address(this));
+        if (_amount > TANBal) {
+            TAN.transfer(_to, TANBal);
+        } else {
+            TAN.transfer(_to, _amount);
+        }
+    }
+}
